4499823734b91516526bb5895166c210831efc80
Lauren Escobedo <lescobedo4@mail.stmarytx.edu>
2022-06-13 01:26:37 -0500

    Working on 4

--- 3_largestPrimeFactor.cpp
+++ 3_largestPrimeFactor.cpp
@@ -7,50 +7,53 @@
 //
 
 #include <iostream>
+#include <math.h>
 
 using namespace std;
+
+unsigned primeFactorFinder(unsigned number);
 
 int main()  {
 
   // Variable declarations
   unsigned number, factor;
-  bool prime = false;
 
   // Setting number
   cout << "Enter the number for which you want to find the largest prime factor: ";
   cin >> number;
   cout << endl;
 
-  while(!prime) {
-    // Find next largest factor, starting from the number and working backward for efficiency
-    for(int i=number-1; i>0; i--) {
-      bool flag = false;
+  factor = primeFactorFinder(number);
 
+  cout << "Largest prime factor: " << factor;
+
+  return 0;
+}
+
+unsigned primeFactorFinder(unsigned number) {
+  bool prime = false;
+  unsigned factor;
+
+  while(!prime) {
+    for(int i=number-1; i>0; i--) {
+      bool evenDivisibilityFlag = false;
       // If the number is evenly divisble, it is a factor so move forward with algorithm
       if(number%i == 0) {
         factor = i;
-        // cout << "Factor loop: " << factor << endl; //------------------------Debugging line
 
-        // Prime number checker, if not prime break immediately
+        // Prime number checker, if not prime (evenly divides) break immediately
         for(unsigned i=2; i<factor; i++) {
           if(factor%i == 0) {
-            // cout << "Prime loop: not prime " << prime << endl;//-------------Debugging line
-            flag = true;
+            evenDivisibilityFlag = true;
             break;
           }
         }
-        // End of prime number checker
-        if(flag == false) {
+        if(evenDivisibilityFlag == false) {
           prime = true;
           break;
         }
       }
     }
   }
-
-  cout << "Largest prime factor: " << factor;
-
-
-
-  return 0;
+  return factor;
 }
